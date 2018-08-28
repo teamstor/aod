05e9935015b0adada19f351f70d25c61b20d7686
Hannes Mann <hannesmann2000@gmail.com>
2018-08-28 21:26:33 +0200

    Add water tile

---
+++ RPG/Map/Tiles.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace TeamStor.RPG.Map
+{
+	/// <summary>
+	/// Contains a list of tiles.
+	/// </summary>
+	public static class Tiles
+	{
+		public static class Terrain
+		{
+			public static Tile Water = new Tile(0, Tile.MapLayer.Terrain, "Water", new Point(0, 0));
+		}
+		
+		public static class Decoration
+		{
+			
+		}
+		
+		public static class NPC
+		{
+			
+		}
+		
+		public static class Control
+		{
+			
+		}
+	}
+}
