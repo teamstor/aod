702911be8fb5878bb859f29d086ed22612aa4324
Hannes Mann <hannesmann2000@gmail.com>
2019-03-15 19:43:51 +0100

    Try to fix play.bat

--- play.bat
+++ play.bat
@@ -1,4 +1,5 @@
 git pull
 git submodule update --remote --merge
-C:\Windows\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe AOD/AOD.sln /t:Build /p:Configuration=Release
-start AOD/bin/AOD.exe
+cd AOD
+C:\Windows\Microsoft.Net\Framework\v4.0.30319\MSBuild.exe AOD.sln /t:Build /p:Configuration=Release
+start bin/AOD.exe
