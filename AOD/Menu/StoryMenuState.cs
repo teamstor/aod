8e9b61ab632748f7d313deeae879349270d5d651
Hannes Mann <hannesmann2000@gmail.com>
2019-03-25 09:23:02 +0100

    Print story in StoryMenuState

--- AOD/Menu/StoryMenuState.cs
+++ AOD/Menu/StoryMenuState.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using TeamStor.AOD.Gameplay.World;
 using TeamStor.Engine;
 using TeamStor.Engine.Graphics;
 using SpriteBatch = TeamStor.Engine.Graphics.SpriteBatch;
@@ -16,6 +17,9 @@ namespace TeamStor.AOD.Menu
     {
         private string _story = "";
         private string _writtenStory = "";
+
+        private long _startCount = -1;
+        private float _scroll = 0;
 
         public override void OnEnter(GameState previousState)
         {
@@ -28,10 +32,25 @@ namespace TeamStor.AOD.Menu
 
         public override void Update(double deltaTime, double totalTime, long count)
         {
+            if(InputMap.FindMapping(InputAction.Action).Pressed(Input))
+            {
+                if(_writtenStory.Length != _story.Length)
+                    _writtenStory = _story;
+                else
+                    Game.CurrentState = new WorldState(Map.Load("data/maps/mapt.json"), true);
+            }
+
+            float targetScroll = Math.Min(-_writtenStory.Split('\n').Length * 18 + (18 * 6), 0);
+            _scroll = MathHelper.Lerp(_scroll, targetScroll, (float)deltaTime * 0.5f);
         }
 
         public override void FixedUpdate(long count)
         {
+            if(_startCount == -1)
+                _startCount = count;
+
+            if((count - _startCount) % 3 == 0 && _writtenStory.Length < _story.Length)
+                _writtenStory += _story[_writtenStory.Length];
         }
 
         public override void Draw(SpriteBatch batch, Vector2 screenSize)
@@ -40,18 +59,27 @@ namespace TeamStor.AOD.Menu
 
             batch.Rectangle(new Rectangle(0, 0, (int)screenSize.X, (int)screenSize.Y), Color.Black);
 
-            Font font = Assets.Get<Font>("fonts/Alkhemikal.ttf", true);
+            Font alkhemikal = Assets.Get<Font>("fonts/Alkhemikal.ttf", true);
 
-            int y = 110;
-            foreach(string s in _story.Split('\n'))
+            int y = (int)(110 + _scroll);
+            string[] split = _story.Split('\n');
+            string[] writtenSplit = _writtenStory.Split('\n');
+
+            for(int i = 0; i < writtenSplit.Length; i++)
             {
-                Vector2 measure = font.Measure(16, s);
-                batch.Text(font, 16, s, new Vector2(screenSize.X / 2 - measure.X / 2, y), Color.White);
-                y += (int)measure.Y;
+                Vector2 m = alkhemikal.Measure(16, split[i]);
+                batch.Text(alkhemikal, 16, writtenSplit[i], new Vector2(screenSize.X / 2 - m.X / 2, y), Color.White);
+                y += 18;
             }
 
             batch.Texture(new Rectangle(0, 0, (int)screenSize.X, (int)screenSize.Y), Assets.Get<Texture2D>("menu/story_mask.png"), Color.White);
             batch.Texture(new Vector2(screenSize.X / 2 - 160 / 2, 20), Assets.Get<Texture2D>("ui/logo.png", true), Color.White);
+
+            Font bitcell = Assets.Get<Font>("fonts/bitcell.ttf", true);
+            string s = "Press [" + InputMap.FindMapping(InputAction.Action).Key + "] to " + (_writtenStory.Length == _story.Length ? "start game" : "skip");
+            Vector2 measure = bitcell.Measure(16, s);
+            float alpha = 0.8f + (float)Math.Sin(Game.Time * 10) * 0.1f;
+            batch.Text(bitcell, 16, s, new Vector2(screenSize.X / 2 - measure.X / 2, screenSize.Y - 8 - 20), Color.White * alpha);
 
             Program.BlackBorders(batch);
         }
